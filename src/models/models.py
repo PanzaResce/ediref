8090bc076d984b3c2d7b62e98534c67c02cfcf6d
MihailStamenov98 <mpstamenov@gmail.com>
2024-04-10 21:51:01 +0300

    fix the tags shape in trainer

--- src/models/models.py
+++ src/models/models.py
@@ -101,19 +101,24 @@ class BERT_Model_Phrase_Extraction(BertPreTrainedModel):
             print("load = ", load)
             return BertModel.from_pretrained(load, config=load+'/config.json', local_files_only=True)
         
-    def search_for_chanels_in_output(input, index, output=None):
-        start_chanel=None
-        end_chanel=None
+    def find_start_chanel(input, index):
         for i, token in enumerate(input):
             if token == 102:
                 index = index-1
             if index == 0:
-                start_chanel = i
-        for i, token in enumerate(input[start_chanel:]):
-            if token == 102:
-                end_chanel = i
+                return i+1
+        return None
+    def find_end_chanel(input, start_index):
+        for i in range(start_index, len(input)):
+            if input[i] == 102:
+                return i
+        return None
+    
+    def search_for_chanels_in_output(self, input, index, output):
+        start_chanel= self.find_start_chanel(input, index)
+        end_chanel=self.find_end_chanel(input, start_chanel)
         print(input[start_chanel:end_chanel])
-        #return output[start_chanel:end_chanel]
+        return output[start_chanel:end_chanel]
             
     def forward(
         self,
@@ -125,18 +130,16 @@ class BERT_Model_Phrase_Extraction(BertPreTrainedModel):
         triggers=None,
         return_dict=None,
     ):
-        #return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-        ##self.search_for_chanels_in_output(input[0], utterance_index[i], outputs[i])
-        ##outputs = self.core(
-        ##    input_ids=dialogue_ids,
-        ##    attention_mask=dialogue_mask,
-        ##    token_type_ids=token_type_ids,
-        ##    return_dict=return_dict,
-        ##) 
-##
-        #model_output = [self.search_for_chanels_in_output(input[i], utterance_index[i], outputs[i]) for i in range(len(dialogue_ids))]
-        #emotion_logits = torch.mean(self.emotion_head(model_output), dim=(1))
-        #trigger_logits = torch.mean(self.trigger_head(model_output), dim=(1))
-        #return {"emotion_logits": emotion_logits,
-        #        "trigger_logits": trigger_logits}
-        pass
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        outputs = self.core(
+            input_ids=dialogue_ids,
+            attention_mask=dialogue_mask,
+            token_type_ids=token_type_ids,
+            return_dict=return_dict,
+        ) 
+
+        model_output = [self.search_for_chanels_in_output(input[i], utterance_index[i], outputs[i]) for i in range(len(dialogue_ids))]
+        emotion_logits = torch.mean(self.emotion_head(model_output), dim=(1))
+        trigger_logits = torch.mean(self.trigger_head(model_output), dim=(1))
+        return {"emotion_logits": emotion_logits,
+                "trigger_logits": trigger_logits}
